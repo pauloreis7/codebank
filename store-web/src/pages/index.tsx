02b33359a94bf40a364420515f10b5b588e57663
pauloreis7 <paulosilvadosreis2057@gmail.com>
2022-08-20 23:00:28 -0300

    build(store-web): setup initial store web nextjs app
    setup nextjs project for store frontend app

---
+++ store-web/src/pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+
+const Home: NextPage = () => {
+  return (
+    <main>
+      <Head>
+        <title>CodeBank</title>
+        <meta name="description" content="CodeBank Home" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <h1>Hello World!</h1>
+      </main>
+    </main>
+  );
+};
+
+export default Home;
