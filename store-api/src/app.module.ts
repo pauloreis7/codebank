e6785b24f49d89a5f599e4974c73d7c856199fa4
pauloreis7 <paulosilvadosreis2057@gmail.com>
2022-08-22 10:52:58 -0300

    feat(store-api): create initial orders nestjs resources
    setup nestjs orders resources

--- store-api/src/app.module.ts
+++ store-api/src/app.module.ts
@@ -3,9 +3,10 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ProductsModule } from './products/products.module'
+import { OrdersModule } from './orders/orders.module'
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService]
 })
