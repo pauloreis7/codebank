0fa73b43187272c88fd34b53923b6bacc6b98e41
pauloreis7 <paulosilvadosreis2057@gmail.com>
2022-08-26 16:22:55 -0300

    feat(invoices-web): setup chakra-ui for styles

--- invoices-web/src/pages/_app.tsx
+++ invoices-web/src/pages/_app.tsx
@@ -1,7 +1,19 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
+import { ChakraProvider } from '@chakra-ui/react'
+
+import { theme } from '../styles/theme'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ChakraProvider resetCSS theme={theme}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
 }
 
 export default MyApp
---
+++ invoices-web/src/styles/theme.ts
@@ -0,0 +1,27 @@
+import { extendTheme, theme as chakraTheme } from '@chakra-ui/react'
+
+export const theme = extendTheme({
+  fonts: {
+    body: 'Inter, system-ui, sans-serif',
+    heading: 'Lexend, system-ui, sans-serif',
+    mono: 'Menlo, monospace'
+  },
+  colors: {
+    gray: {
+      ...chakraTheme.colors.gray,
+      300: '#e1e1e6',
+      600: '#29292e',
+      700: '#202024',
+      800: '#121214',
+      900: '#151515'
+    }
+  },
+  styles: {
+    global: {
+      body: {
+        bg: 'gray.800',
+        color: 'gray.50'
+      }
+    }
+  }
+})
