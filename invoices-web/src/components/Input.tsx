0f7009f9d7e0876034a2b4d1af9dee7e608edee4
pauloreis7 <paulosilvadosreis2057@gmail.com>
2022-08-28 20:12:26 -0300

    feat(invoices-web): create login page layout
    create page to get credit card number to find invoices

---
+++ invoices-web/src/components/Input.tsx
@@ -0,0 +1,58 @@
+import {
+  FormControl,
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+  InputGroup,
+  InputLeftElement,
+  Icon as ChakraIcon
+} from '@chakra-ui/react'
+import { IconType } from 'react-icons'
+
+interface InputProps extends ChakraInputProps {
+  icon?: IconType
+  value: string
+  setValue: (value: string) => void
+}
+
+export function Input({ icon: Icon, value, setValue, ...rest }: InputProps) {
+  return (
+    <FormControl isInvalid={false}>
+      <InputGroup w="100%" h="3.125rem">
+        <InputLeftElement h="100%">
+          <ChakraIcon
+            as={Icon}
+            color={value.length === 16 ? 'yellow.500' : 'gray.600'}
+            width="1.25rem"
+            height="1.25rem"
+          />
+        </InputLeftElement>
+
+        <ChakraInput
+          w="100%"
+          h="100%"
+          color="gray.100"
+          bgColor="gray.800"
+          variant="filled"
+          borderColor="gray.800"
+          borderRadius="sm"
+          value={value}
+          onChange={event => setValue(event.target.value)}
+          _hover={{
+            borderColor: 'yellow.500'
+          }}
+          _placeholder={{
+            color: 'gray.500'
+          }}
+          _focus={{
+            bgColor: 'gray.800',
+            borderColor: 'yellow.500'
+          }}
+          _invalid={{
+            borderColor: 'red.500'
+          }}
+          {...rest}
+        />
+      </InputGroup>
+    </FormControl>
+  )
+}
