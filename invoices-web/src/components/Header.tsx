16e84bbade170c074d4d6c215e1e36da2b3cbe2b
pauloreis7 <paulosilvadosreis2057@gmail.com>
2022-08-28 19:57:19 -0300

    feat(invoices-web): create Header component

---
+++ invoices-web/src/components/Header.tsx
@@ -0,0 +1,36 @@
+import { Link as ChakraLink, Flex, Icon, Text } from '@chakra-ui/react'
+import { BiStore } from 'react-icons/bi'
+import Link from 'next/link'
+
+type HeaderProps = {
+  href?: string
+}
+
+export function Header({ href = '/' }: HeaderProps) {
+  return (
+    <Flex
+      as="header"
+      w="100%"
+      px={['4', '8']}
+      backgroundColor="yellow.600"
+      boxShadow="dark-lg"
+    >
+      <Link href={href} as={href} passHref>
+        <ChakraLink
+          display="flex"
+          alignItems="center"
+          py="4"
+          _hover={{
+            textDecoration: 'none'
+          }}
+        >
+          <Icon as={BiStore} mr="2" fontSize="3xl" color="gray.300" />
+
+          <Text fontSize="2xl" fontWeight="bold">
+            CodeBank
+          </Text>
+        </ChakraLink>
+      </Link>
+    </Flex>
+  )
+}
